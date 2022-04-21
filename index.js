2b95d79890e1db540f32c43471fbe8d5e734cbb0
Diogo <diogo.mvieira@gmail.com>
2022-04-20 22:05:34 -0300

    Adding static files

---
+++ index.js
@@ -0,0 +1,35 @@
+let map;
+
+function initMap() {
+  const rio = new google.maps.LatLng(-22.958296, -43.257151);
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: rio,
+    zoom: 12
+  });
+
+  fetch("./places.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(places){
+        for (var i in places) {
+            var place = places[i];
+            createMarker(place.name, place.geo);
+        }
+    })
+
+}
+
+function createMarker(name, geo) {
+    
+
+  new google.maps.Marker({
+    map,
+    position: geo,
+    title: name
+  });
+
+}
+
+window.initMap = initMap;
